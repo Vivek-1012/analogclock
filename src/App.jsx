cd8ac28f153550bbae1495029fa33bbb2a4c7180
Vivek Kumar <110529479+Vivek-1012@users.noreply.github.com>
2023-03-23 03:27:51 +0530

    doc

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Clock from './Components/Clock.jsx'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Clock />
+      
+    </div>
+  )
+}
+
+export default App ;
